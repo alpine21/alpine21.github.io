668ddb9aa73ebca32d9280591700125a3fc66af3
Rufis Mosengo <98060139+alpine21@users.noreply.github.com>
2022-07-20 20:52:51 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
   // = = = = = = = = = = Text Animation = = = = = = = = = = //
 
   var typed = new Typed(".text-home", {
-    strings: ["DUT", "R&T."],
+    strings: ["DUT", "R&T2."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
